80e1f76e5bd731fa35f3817a3f22a62ce205549c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-07 23:47:11 -0300

    Add a new file for showing all the primitive types in rust

---
+++ all-about-rust/src/types.rs
@@ -0,0 +1,9 @@
+/*
+Primitive Types
+    Integers: u8, i8, u16, u32, i32, i64, u128, i128 <-- These refer to the number of bits that take in memory
+    Floats: f32, f64;
+    Boolean: true | false;
+    Characters: char;
+    Tuples: a type of array, but different to the array itself, regarding how it works;
+    Arrays: these arrays are fixed in length and cannot be changed;
+*/
