257646d688ac2478b5ee2eb59f35cd59978eaaa4
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-12 00:21:26 -0300

    Insert a shorthand for if statements

--- all-about-rust/src/conditionals.rs
+++ all-about-rust/src/conditionals.rs
@@ -1,5 +1,5 @@
 pub fn run() {
-    let person: (&str, &str, i8) = ("Elias", "Pereyra", 16);
+    let person: (&str, &str, i8) = ("", "Pereyra", 16);
 
     if person.0 != "" && person.1 != "" && person.2 > 18 {
         println!("Elias, you are already an adult. You can go on")
@@ -8,4 +8,8 @@ pub fn run() {
     } else {
         println!("Show me your credentials to verify");
     }
+
+    // shorthand for if statements
+    let verify_name = if person.0 == "" { false } else { true };
+    println!("Does the person have a name? {}", verify_name);
 }
