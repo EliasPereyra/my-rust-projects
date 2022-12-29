2f86633f743c266fa83dd1107a011e2eceec1618
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-12-28 21:19:49 -0300

    Add a new definition explaining more precisely about vectors

--- all-about-rust/src/datatypes/vectors.rs
+++ all-about-rust/src/datatypes/vectors.rs
@@ -18,7 +18,11 @@ pub fn run() {
         nums
     );
 
-    // Vectors are heaped allocated
+    /*  Vectors are heaped allocated. The online book or the e-book says, quote:
+        "Unlike the built-in array and tuple types, the data these collections point to
+        is stored on the heap, which means the amount of data does not need to be known
+        at compile time and can grow or shrink as the program runs."
+    */
     println!(
         "Vectors are heaped allocated, they occupy {} bytes",
         mem::size_of_val(&nums)
