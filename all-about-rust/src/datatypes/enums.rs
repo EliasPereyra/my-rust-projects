932d65bd04991e86c169b5bad008dae3170386c5
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-03-25 23:34:19 -0300

    Fix typos regarding variable names

--- all-about-rust/src/datatypes/enums.rs
+++ all-about-rust/src/datatypes/enums.rs
@@ -7,6 +7,16 @@ enum Movement {
     FlyLeft,
 }
 
+fn move_plane(m: Movement) {
+    // Perform action depending on data received
+    match m {
+        Movement::FlyUp => println!("The plane is flying up"),
+        Movement::FlyDown => println!("The plane is flying down"),
+        Movement::FlyRight => println!("The plane is flying to the right"),
+        Movement::FlyLeft => println!("The plane is flying to the left"),
+    }
+}
+
 pub fn run() {
     let plane_goes_up = Movement::FlyUp;
     let plane_goes_down = Movement::FlyDown;
@@ -17,14 +27,4 @@ pub fn run() {
     move_plane(plane_goes_down);
     move_plane(plane_goes_right);
     move_plane(plane_goes_left);
-}
-
-fn move_plane(m: Movement) {
-    // Perform action depending on data received
-    match m {
-        Movement::FlyUp => println!("The plane is flying up"),
-        Movement::FlyDown => println!("The plane is flying down"),
-        Movement::FlyRight => println!("The plane is flying to the right"),
-        Movement::FlyLeft => println!("The plane is flying to the left"),
-    }
 }
