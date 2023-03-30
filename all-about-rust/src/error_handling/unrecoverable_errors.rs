81733fa967df1f605e00c2156652fb71280736b6
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-03-29 23:53:10 -0300

    Update the file with more information about unrecoverable errors

--- all-about-rust/src/error_handling/unrecoverable_errors.rs
+++ all-about-rust/src/error_handling/unrecoverable_errors.rs
@@ -1,5 +1,15 @@
 // Unrecoverable errors
 
 pub fn run() {
-    panic!("Crash and burn!")
+    // basic panic
+    // panic!("Crash and burn!")
+
+    // Going beyond the length of a vector will cause a panic
+    let vec = [1, 2, 3];
+
+    // In the terminal output, rust will tell you exactly where the problem is
+    // Even more, you can write: RUST_BACKTRACE=1 cargo run for getting more info
+    // It gives you a long list, very detailed information of how was the compilation executed
+    // Note: the result of the backtrace may vary depending on your OS and rust version
+    vec[10]; // <-- will panic
 }
