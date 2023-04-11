eff3c00adde094e4cd4ce0a3c2d066b0924f48eb
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-04-10 23:53:07 -0300

    Implement a customized type for validation for integer inputs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,44 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!(
+                "The number is out of range. You should enter a value between 1 and 100, {}",
+                value
+            );
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=1000);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut guess);
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        
+
+        Guess::new(guess);
+
         println!("Please, input your guess.");
         println!("You guessed: {guess}");
 
@@ -28,7 +48,7 @@ fn main() {
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            },
+            }
         }
-    }   
+    }
 }
