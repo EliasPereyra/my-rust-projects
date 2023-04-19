07e4b09285569e542daaaf474346b1cfc1a623ba
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-04-18 23:36:04 -0300

    Implement a simple trait in the main func by now

--- all-about-rust/src/main.rs
+++ all-about-rust/src/main.rs
@@ -6,6 +6,36 @@ use crate::garden::vegetables::Asparagus;
 
 pub mod garden;
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{} {}", self.username, self.content)
+    }
+}
+
 fn main() {
     // print::run();
     // datatypes::vars::run();
@@ -19,10 +49,19 @@ fn main() {
     // structs::run();
     // enums::run();
     // datatypes::hashmaps::run();
-    // let plant = Asparagus {};
-    // println!("I'm growing {:?}", plant);
+    let plant = Asparagus {};
+    println!("I'm growing {:?}", plant);
     // error_handling::unrecoverable_errors::run();
     // error_handling::recoverable_errors::run();
     // error_handling::recoverable_errors::read_email_from_file();
-    generic_types::remove_duplications::run();
+    // generic_types::remove_duplications::run();
+
+    let tweet = Tweet {
+        username: String::from("tech_news"),
+        content: String::from("Big companies are hacked very often"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
